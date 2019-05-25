a96fc97f311e7b67a82e9ad1a3499bea4bc25118
Max Haughton <maxhaton@gmail.com>
2019-05-25 01:50:33 +0100

    Travis

--- source/util/string.d
+++ source/util/string.d
@@ -1,2 +1,19 @@
 ///Nogc (Managed string) string 
-module manstring;
+module string;
+import stdx.allocator;
+public alias utf8 = char;
+public alias utf16 = wchar;
+public alias utf32 = dchar;
+@nogc:
+@safe:
+
+struct manString {
+
+	union _SmlStr {
+		///32 chars for small string optimization
+		dchar[31] small;
+
+	}
+}
+
+
