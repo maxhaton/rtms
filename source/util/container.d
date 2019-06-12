eb4e490468516df03a5f98e38cce2488bc1a0dbe
Max Haughton <maxhaton@gmail.com>
2019-06-12 23:54:59 +0100

    dub.json dip1008

--- source/util/container.d
+++ source/util/container.d
@@ -1,13 +1,15 @@
-///Containers, currently just covers the GC.
 module util.container;
-///Currently just wraps a normal array for later replacement.
+@nogc: 
+@safe: 
 import util.allocate;
 /*
 A managedArray type, constructs objects and handles thread safety 
 if Type is shared.
 */
 struct rtmArray(Type, Allocator) {
-	
+	private {
+		Type[] data;
+	}
 	
 	
 }
